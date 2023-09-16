4dd2ae82a25aedecf80dc5c7641731b1f11317ae
ziety <officialneweramusic@gmail.com>
2023-09-16 13:43:21 +1200

    first commit

---
+++ doc.go
@@ -0,0 +1,9 @@
+package main
+
+// zcoin
+//
+// - ED25519 crypto
+// - UTXO model
+// - Protobuffer encoding
+// - GRPC transport (gossip)
+// POS consensus (to be determined...)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ziety/zcoin
+
+go 1.21.0
